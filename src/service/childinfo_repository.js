f39a816d19768dce6f82f8cd9a83b9842edf9f86
chestnut1044 <wbk1620@gmail.com>
2021-09-26 22:12:19 +0900

    ..

--- src/service/childinfo_repository.js
+++ src/service/childinfo_repository.js
@@ -3,22 +3,24 @@ import firebaseapp from './firebase'
 class ChildRepository{
   searchInfo(userId){
     console.log("서치인포 실행됨", userId)
+    let result;
     var info = firebaseapp.database().ref(`${userId}`);
-    info.on('value', snapshot =>{
+    info.once('value', snapshot =>{
       var value = snapshot.val()
       if(value!=null){
         console.log("value가 null 이 아닐때 값", value);
-        var result = true;
+        result = true;
       }else{
         console.log("value가 null",value);
-        var result = false;
+        result = false;
       }
-      console.log("출력값", result)
+      console.log("출력값1", result)
     })
-    // console.log(result)
+    console.log("출력값2",result)
+    return result
   }
 
-  
+
   saveinfo(userId, kindergarten){
     console.log("정보 저장 실행됨")
     console.log("유저아이디 : ", userId, "어린이집정보", kindergarten)
